01e10f6f3fb88570be5f49df788d60f233f48c50
YassineMest <yassinemestaoui98@gmail.com>
2022-05-08 12:11:49 +0100

    adsdas

--- controle/mestaoui_yassine/urls.py
+++ controle/mestaoui_yassine/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listeComptes, name='allcomptes'),
-    path('client/', views.listeClients, name='allclients'),
+    path('', views.home, name='home'),
+    path('admin/', views.admin, name='admin'),
+    path('client/', views.allClients, name='allclients'),
+    path('client/add', views.view_client, name='addclient'),
     path('client/<int:code>', views.getClient, name='getclient'),
     path('client/search/', views.searchClient, name='searchclient'),
-    path('operation/<int:numero>', views.listeOperationsClient, name='getoperationclient'),
-    path('client/add', views.view_client, name='addclient'),
+    path('operation/<int:numero>', views.operationsClient, name='operationsclient'),
+    path('compte/', views.allComptes, name='allcomptes'),
     path('compte/add', views.view_compte, name='addcompte'),
+    path('compte/search/', views.searchCompte, name='searchcompte'),
     path('operation/add', views.view_operation, name='addoperation'),
 ]
--- controle/mestaoui_yassine/views.py
+++ controle/mestaoui_yassine/views.py
@@ -6,18 +6,14 @@ from .forms import ClientForm, CompteForm, OperationForm
 from .models import Compte, Client, Operation
 
 
-def listeComptes(request):
-    comptes = Compte.objects.all()
-    paginator = Paginator(comptes, 10)
-    page_number = request.GET.get('page')
-    if page_number is not None:
-        pageObj = paginator.get_page(page_number)
-    else:
-        pageObj = paginator.get_page(1)
-    context = {'comptes': pageObj}
-    return render(request, 'base.html', context)
+def home(request):
+    return render(request, 'base.html')
 
-def listeClients(request):
+def admin(request):
+    return render(request, 'admin/')
+
+
+def allClients(request):
     clients = Client.objects.all()
     paginator = Paginator(clients, 10)
     page_number = request.GET.get('page')
@@ -28,9 +24,10 @@ def listeClients(request):
     context = {'clients': pageObj}
     return render(request, 'clients.html', context)
 
+
 def searchClient(request):
-    filteredClients = Client.objects.filter(nom__icontains=request.GET.get("search", ""))
-    paginator = Paginator(filteredClients, 10)
+    searchedClients = Client.objects.filter(nom__icontains=request.GET.get("search", ""))
+    paginator = Paginator(searchedClients, 10)
     page_number = request.GET.get('page')
     if page_number is not None:
         page_obj = paginator.get_page(page_number)
@@ -39,28 +36,53 @@ def searchClient(request):
     context = {'clients': page_obj}
     return render(request, "clients.html", context)
 
+
 def getClient(request, numero):
     client = Client.objects.get(numero=numero)
     context = {'client': client}
-    return render(request, 'base.html', context)
+    return render(request, 'clients.html', context)
 
 
-def listeOperationsClient(request, numero):
+def operationsClient(request, numero):
     compte = Compte.objects.get(client=numero)
     operations = Operation.objects.get(compte=compte.numero)
     context = {'operations': operations}
-    return render(request, 'base.html', context)
+    return render(request, 'clients.html', context)
+
+
+def allComptes(request):
+    comptes = Compte.objects.all()
+    paginator = Paginator(comptes, 10)
+    page_number = request.GET.get('page')
+    if page_number is not None:
+        pageObj = paginator.get_page(page_number)
+    else:
+        pageObj = paginator.get_page(1)
+    context = {'comptes': pageObj}
+    return render(request, 'comptes.html', context)
+
+def searchCompte(request):
+    searchedCompte = Compte.objects.filter(numero__icontains=request.GET.get("search", ""))
+    paginator = Paginator(searchedCompte, 10)
+    page_number = request.GET.get('page')
+    if page_number is not None:
+        page_obj = paginator.get_page(page_number)
+    else:
+        page_obj = paginator.get_page(1)
+    context = {'comptes': page_obj}
+    return render(request, "comptes.html", context)
+
 
 def view_client(request):
     form = ClientForm()
     return render(request, "formTemplate.html", {"form": form})
 
+
 def view_compte(request):
     form = CompteForm()
     return render(request, "formTemplate.html", {"form": form})
 
+
 def view_operation(request):
     form = OperationForm()
     return render(request, "formTemplate.html", {"form": form})
-
-
